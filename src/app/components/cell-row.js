a1718aef8e5533150216be70d8b700fe841ccc8a
Arcia125 <tauren125@gmail.com>
2016-10-06 13:18:47 -0500

    add clear increase performance
    Cleaned up code and increased performance by converting some components to
    functional stateless components. Added a condition for cell components to
    update in which they must receive a new className (cell_dead, cell_alive).
    Added funcitonality to the clear button.

--- src/app/components/cell-row.js
+++ src/app/components/cell-row.js
@@ -1,14 +1,12 @@
 const React = require('react');
 
-class CellRow extends React.Component {
-	render() {
-		let props = this.props;
-		return (
-			<div id={'row-' + props.rowID} className='cell-row'>
-				{props.children}
-			</div>
-			);
-	}
+
+const CellRow = (props) => {
+	return(
+		<div id={'row-' + props.rowID} className='cell-row'>
+			{props.children}
+		</div>
+		);
 }
 
 export default CellRow;
--- src/app/components/menu.js
+++ src/app/components/menu.js
@@ -2,19 +2,16 @@
 
 const React = require('react');
 
-class Menu extends React.Component {
-	render() {
-		let props = this.props;
-		let classes = [];
-		if (props.className) {
-			classes.push(props.className);
-		}
-		classes.push('menu');
-		return(
+const Menu = (props) => {
+	let classes = [];
+	if (props.className) {
+		classes.push(props.className);
+	}
+	classes.push('menu');
+	return (
 		<div className={classes.join(' ')}>
 			{props.children}
 		</div>
 		);
-	}
 }
 export default Menu;
