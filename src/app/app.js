b21ac91b8d9c880156a1e2dded23c1fa98ca3e62
Arcia125 <tauren125@gmail.com>
2016-10-08 16:01:33 -0500

    add header with information tab

--- src/app/app.js
+++ src/app/app.js
@@ -14,7 +14,8 @@ export default class App extends React.Component {
 			randomize: false,
 			density: 50,
 			generation: 0,
-			interval: null
+			interval: null,
+			info: false
 		}
 	}
 
@@ -100,9 +101,50 @@ export default class App extends React.Component {
 			});
 		}, 10);
 	}
+	displayInfo() {
+		this.setState({
+			info: !this.state.info
+		});
+	}
 	render() {
 		return (
 			<div className='app'>
+				<Menu className='info-menu'>
+					<h1 className='info-menu_title'>
+						John Conway's Game of Life
+					</h1>
+					<h2 className='info-menu_subtitle'>
+						Built with React by Kevin Hallett
+					</h2>
+					<Button onClick={() => this.displayInfo()} className='info-button'>More Info</Button>
+					<div className={'info-menu_panel' + (this.state.info ? ' show' : '')}>
+						<section className='panel-content'>
+							<h2 className='panel-content_header'>The Game</h2>
+							<p className='panel-content_para'>
+								<a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'>The Game of Life</a> was
+								invented by a Cambridge mathematician
+								named <a href='https://en.wikipedia.org/wiki/John_Horton_Conway'>John Conway</a> in
+								1970. The game really doesn't have any players, John Conway calls it a "no-player game."
+								It is actually
+								a <a href='https://en.wikipedia.org/wiki/Cellular_automaton'>cellular automaton</a> which
+								is basically a grid of cells that can have a finite number of states (e.g. alive or dead)
+								that follow a set of rules to determine their state in future generations.
+							</p>
+							<h2 className='panel-content_header'>The Rules</h2>
+							<p className='panel-content_para'>
+								All cells can either be alive (shades of green) or dead (black). Each cell
+								has a neighborhood of consisting of every cell touching it.
+							</p>
+							<p className='panel-content_para'>
+								Living cells with a light green color are born more recently than those
+								that are a darker green. Living cells will die of loneliness if they have one
+								or less living neighbors. If a living cell has four or more neighbors it dies
+								of overcrowding. Dead cells can only be born in the next generation if they
+								have exactly 3 living neighbors.
+							</p>
+						</section>
+					</div>
+				</Menu>
 				<Menu className='top-menu'>
 					<Button onClick={() => this.handleRun()} className='run-button' >Run</Button>
 					<Button onClick={() => this.handlePause()} className='pause-button' >Pause</Button>
